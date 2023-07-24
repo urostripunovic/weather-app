721213005d5af78c3b387cc59848014782558a9f
urostripunovic <uros_1997@hotmail.com>
2023-07-24 20:55:12 +0200

    Update WeatherPage.jsx

--- src/WeatherPage.jsx
+++ src/WeatherPage.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import "./WeatherPage.css";
 
 const WeatherPage = ({ data }) => {
-    // Your WeatherPage component logic here
-    // ...
     console.log(data)
 
     return (
